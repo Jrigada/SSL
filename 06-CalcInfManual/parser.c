15cda479004884081e03a0d3704a9ddb45692f4a
jrigada <jrigada@est.frba.utn.edu.ar>
2021-02-13 13:45:03 -0300

    calculadora automatica finalizada, calculadora manual falta la funcion de ejecutar mas de un calculo por ejecucion

--- 06-CalcInfManual/parser.c
+++ 06-CalcInfManual/parser.c
@@ -60,10 +60,6 @@ token tempToken;
             insertArray(&a,tempToken);
         }
     }
-//leo el array para chequear si es correcto
-    // int i;
-    // for (i=0;i<a.size;i++)
-    //     printf("%d\n", a.array[i]);
     
     fclose(filePointer);
 
@@ -274,35 +270,6 @@ void expresionRightPar(void){
         break;
     }
 }
-
-// void expresion(void){
-//     switch (currentToken) 
-//     {
-//     case t_id:
-//         match(t_id);
-//         while (currentToken == t_id)
-//         {
-//             match(t_id);
-//         }
-//         operador();
-//         break;
-    
-//     case t_constNum:
-//         match(t_constNum);
-//         operador();
-//         break;
-    
-//     case t_rightpar:
-//         match(t_rightpar);
-//         expresionLeftPar();
-//         break;
-    
-//     default:
-//         printf("%s\n","fallo 3");
-//         break;
-//     }
-// }
-
 void operador(void){
     switch (currentToken)
     {
@@ -408,63 +375,3 @@ void operadorInsidePar(void){
         break;
     }
 }
-
-
-//     token tokens[30];
-
-
-//     int tokenIndex = 0;
-//     char str[100];
-     
-//     printf("Ingresa una expresion: ");
-//     gets(str);
-
-     
-//     size_t i = 0;
-    
-//     while (str[i] != '\0') {      
-//         tokens[tokenIndex] = getNextToken(str[i]); 
-//         i++;
-//     } 
-         
-//     result = parsear(tokens,tokenIndex);
-    
-//     if (result == true) {
-//         printf("%s\n","Expresion valida");
-//     } else {
-//         printf("%s\n","Expresion invalida");
-//     }
-
-//     return 0;
-// }
-
-// bool parsear(token *tokens, int token_len) {
-//     int i;
-//     bool valid_exp;
-//     token token_anterior = t_null;
-
-//     for ( i=0 ; i<=token_len ; i++ ){
-//         if (token_anterior == t_null){
-//             if (tokens[i] == t_mul || tokens[i] == t_sum) {
-//                 valid_exp = false; 
-//                 break;
-//             } else {
-//                 token_anterior = tokens[i];
-//                 };
-//         } else if (!esParValido(token_anterior,tokens[i])) {
-//         valid_exp = false;
-//         break;
-//         } else {
-//             valid_exp = true;
-//         }
-//     }
-//     return valid_exp;
-// }
-
-// bool esParValido(token a, token b) {
-//     if ((a == t_mul && b == t_sum)||(a == t_sum && b == t_mul)) {
-//         return false;
-//     } else {
-//         return true;
-//         }
-// }
